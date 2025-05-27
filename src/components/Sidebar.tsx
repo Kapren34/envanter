4b26cfb9523a6a8e3cff4409f203617b683a1675
Kapren34 <kapren34@gmail.com>
2025-05-27 17:16:01 +0300

    Deploy application to Netlify

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -102,7 +102,6 @@ const Sidebar = () => {
           <div className="flex items-center text-indigo-200">
             <Monitor className="h-4 w-4 mr-1" />
             <span>Görüntü</span>
-            <span>Desing by YILDIRIM</span>
           </div>
         </div>
       </div>
